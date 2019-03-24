1ce2157ec5144c0ffc25a10baf38434e48d70c92
liuhongxia <310731483@qq.com>
2019-03-24 12:54:21 +0800

    修改APPID

--- data/posts-data.js
+++ data/posts-data.js
@@ -12,7 +12,7 @@ var local_database = [{
   deteTime: "3天前",
   detail: "我和我的导师曾做过对大学生群体的微信语音与文字交流偏好研究.\n\n因此，我们可以先来看下大学生这个年轻群体对微信语音交流的看法。研究过程采用了开放式笔谈的形式，调查对象为北京某高校来自各学院 113 名本科生。开放式笔谈，即人们在面对面的情况下不使用口头语言（对话、交谈），而是通过互相书面书写的方式，来传达彼此的意思。\n\n相对于一般的对话交谈而言，笔谈传达讯息的效率相对较低，但精确度则相对较高。",
   music: {
-    dataUrl: 'http://157.255.154.152/amobile.music.tc.qq.com/C400003YC3p31HyR96.m4a?guid=1826597270&vkey=58990A24D6287E387EC6C3613883D00365829DEEF20D263077B74468E378F466F132391F4F1D8C8E874BD034BFCAD13FEB29592AE2E9C26F&uin=0&fromtag=66',
+    dataUrl: 'http://isure.stream.qqmusic.qq.com/R400001AisPJ2K2hKv.m4a?guid=1826597270&vkey=D3BA72DD67A5F6426BDE312716D0F40C3C99C349FBB79F0E9A698BADE28EF2FC92FFFF3EB12630C18122514114A317A69889D1EEE076E591&uin=0&fromtag=66',
     title: '最美的期待-周笔畅',
     coverImgUrl: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000001Qn04n29RAmP.jpg?max_age=259'
   }
@@ -30,7 +30,7 @@ var local_database = [{
   deteTime: "3天前",
   detail: "联通这是要作死吗？这么做，用户能不能真正获益呢?\n\n我认为用户会真正获益。首先，联通移动竞争，包括电信也在加大优惠力度，必然会带来资费下降，给用户带来实实在在的利益。腾讯王卡就是一个很好的例子，竞争让移动电信都跟着推出了无限流量卡，用户的资费确实降下来了。\n\n第二，竞争也是满足国家提速降费的指标要求。工信部再次明确了今年下半年将重点落实家庭宽带价格将要下降30%等目标，运营商是必须要满足了，顺便打一波价格战，还可以讨好用户，一举三得。 第三，这种免费送宽带不可能全国大范围推广，而是运营商根据不同地区的竞争态势进行的调整。所以，对运营商自身的伤害，也是可控的。",
     music: {
-      dataUrl: 'http://157.255.154.159/amobile.music.tc.qq.com/C400000Md1wq0vnwzE.m4a?guid=1826597270&vkey=79686E324ED1BD8D71149EFF438ED6D0D9464A1267889EA62D41F4BB86101DDA1D8D704341683AB2121BDE83FA5E24DED053E1B0D358D741&uin=0&fromtag=66',
+      dataUrl: 'http://112.90.152.149/amobile.music.tc.qq.com/R400004eT3P91pKNsm.m4a?guid=1826597270&vkey=84ED06DC594D257BAE32BAD90FE9E7A2FE94D83540EE33DF609C2A813C8C8CA71F51227F30C6640BE0DF25272CF700FE5211F3AFA770BC25&uin=0&fromtag=66',
       title: '体面-于文文',
       coverImgUrl: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000001qYTzY2oyDyZ.jpg?max_age=2592000'
     }
@@ -48,7 +48,7 @@ var local_database = [{
   deteTime: "2天前",
   detail: "从2015年到2018年，每隔一段时间，这个消息便会卷土重来，发生的时间节点通常是：4G发牌、财报公布、5G上马，尤其随着中国移动愈加“一家独大”，“电联”联手的可能性似乎变得更大了。\n\n不可否认，从实操层面看，中国电信和中国联通合并困难重重，两者历经多次电信重组，人员、组织、结构庞杂，且业务线几乎完全重合，国内外类似体量的企业合并，都会带来巨大人事震荡；其次，两者股权结构不同，尤其是中国联通，既在A股和港股两地上市，又是国企混改试点，BATJ资本进入、员工持股都已基本完成，而中国电信则相对简单，一旦合并，如何向资本市场交代，也是件极复杂的事。然而，中国通信市场上，很多不可能已经变成了可能，2008年第三次电信重组，中国电信以1100亿收购了中国联通CDMA网络、中国联通和中国网通合并、中国移动和中国铁通合并，分拆整合动静之大，绝不小于电信联通合并；2014年4月，筹划铁塔公司的消息刚刚传出，7月铁塔便已落地，三家运营商资产剥离，资本市场一样要接受。",
     music: {
-      dataUrl: 'http://112.90.152.146/amobile.music.tc.qq.com/C4000032ZOkm0LBgHW.m4a?guid=1826597270&vkey=BAB414A6B84D9C826368BAC054EBE0BE6A01FCA0457D8DE416AFA88E381EFA2B8CE0D18EABFB314BAE30C7E2D356BBE80F85EFFE7CA13EB4&uin=0&fromtag=66',
+      dataUrl: 'http://isure.stream.qqmusic.qq.com/C400002lN6f00WdYFP.m4a?guid=1826597270&vkey=095F88956B88F33AA0D2A5D821E69AF025C8C6198B1116CD97B7E4DA185EFB417197868767663F6685C0277203A07ABA062B4FECF9C82006&uin=0&fromtag=66',
       title: '违背的青春-薛之谦',
       coverImgUrl: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000003Rtibw458BC5.jpg?max_age=2592000'
     }
@@ -66,9 +66,9 @@ var local_database = [{
   deteTime: "2天前",
   detail: "高通的第三代超声波传感器是高通最新的产品，第一代超声波仪于2015年推出，第二代则在2017年6月首次亮相，这次的第三代也充分满足了三星的要求，同时三星galaxy S10也是首款搭载了高通第三代超声波传感器的设备。\n\n当然，三星S10的主要卖点也将会是这个。大内存水滴屏 OPPO官网低调上线A7x手机 今日OPPO官网上线了一款名为OPPO A7x的智能手机，该手机拥有星空紫、冰焰蓝两种颜色可选，该款手机定位在中端机市场，售价为2099元，9月14日10点正式开售。参数方面，OPPO A7x将使用水滴屏设计，屏幕尺寸为6.3英寸，分辨率为2340 x 1080，屏占比达90.8 %。前置1600万像素AI美颜相机，后置1600万像素AI智能双摄。机身存储选用4GB RAM + 128GB ROM组合，搭载ColorOS 5.2操作系统。",
     music: {
-      dataUrl: 'http://112.90.53.143/amobile.music.tc.qq.com/C4000016EamD3hZN86.m4a?guid=1826597270&vkey=ACD77F781215D0EB11462F4C392AAD6585CBD07460C9EFEF4EE977CFBDF72E1763EFC5170F100D6272B2AFF5C85C437DB0DED89FE70942A0&uin=0&fromtag=66',
-      title: '一百万个可能-Christine welch',
-      coverImgUrl: 'https://y.gtimg.cn/music/photo_new/T002R300x300M000004IWoIx34J0fT.jpg?max_age=2592000'
+      dataUrl: 'http://dl.stream.qqmusic.qq.com/C400000fVqPH1RjwS3.m4a?guid=1826597270&vkey=C6E1C6F6B5CC0E8D97B0475A7AF8B16C5E3EFB8672AD9451A28A51F8271A7BFCC114D953EC56A925BC81D8833D2425978B89BF5FA8B097CD&uin=0&fromtag=66',
+      title: '飞云之下-韩红/林俊杰',
+      coverImgUrl: 'https://y.gtimg.cn/music/photo_new/T001R300x300M000003WlPvN2EvRaj.jpg?max_age=2592000'
     }
 }]
 
